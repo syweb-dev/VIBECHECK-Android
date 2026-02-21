44050bd7bdec79a5709cdb8cdcc574b2a50f7d0b
sanyou-hash <sanyou123666@gmail.com>
2026-02-21 18:03:00 +0800

    修复部分三星手机无法正常安装的问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "top.lucanex.top.vibecheck"
+        applicationId = "top.lucanex.vibecheck.app"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
@@ -22,6 +22,10 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
